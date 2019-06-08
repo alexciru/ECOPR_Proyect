cad58cf1bb24d66e6709775bde1ddbac79dc7f13
alexciru <alejandro.cirugeda@estudiante.uam.es>
2019-06-08 21:11:30 +0200

    fixed

--- main.py
+++ main.py
@@ -12,34 +12,14 @@ from machineReader import *
 
 def main():
     # Read information from the file
-<<<<<<< HEAD
     r = MachineReader()
     r.readFile("data.txt")
 
-=======
-<<<<<<< HEAD
-    r = MachineReader()
-    r.readFile("data.txt")
-
-    print(r.machines[0])
-    print(r.machines[1])
-=======
-    #r = MachineReader()
-    #r.readFile("data.txt")
->>>>>>> 919de858d32d15830aaefb8bb63e185b80062cb7
->>>>>>> 5aca5c16ae827290341d6a538d156d6cc2dfeaf5
     # Obtain a list of machines
     
     # start executing the algoritm
-<<<<<<< HEAD
-=======
     
-<<<<<<< HEAD
     #deadlocks = algorithm()
-=======
->>>>>>> 919de858d32d15830aaefb8bb63e185b80062cb7
-    deadlocks = algorithm()
->>>>>>> 5aca5c16ae827290341d6a538d156d6cc2dfeaf5
     # show how many deadlocks 
  
     return
