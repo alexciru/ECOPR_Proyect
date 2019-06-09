9b94c8f626892cbe55dd0c09d983170de6bc2626
Juancar97 <jcgomezquint@gmail.com>
2019-06-09 14:17:04 +0200

    git commit

--- main.py
+++ main.py
@@ -12,23 +12,11 @@ from machineReader import *
 
 def main():
     # Read information from the file
-<<<<<<< HEAD
     r = MachineReader()
     r.readFile("data.txt")
-
-    print(r.machines[0])
-    print(r.machines[1])
-=======
-    #r = MachineReader()
-    #r.readFile("data.txt")
->>>>>>> 919de858d32d15830aaefb8bb63e185b80062cb7
     # Obtain a list of machines
     
     # start executing the algoritm
-<<<<<<< HEAD
-=======
-    
->>>>>>> 919de858d32d15830aaefb8bb63e185b80062cb7
     deadlocks = algorithm()
     # show how many deadlocks 
  
